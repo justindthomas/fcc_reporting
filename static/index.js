162b0c5fe4b97045a894bdf48179317eca9c0a89
Justin Thomas <justin@jdt.io>
2023-07-18 12:12:54 -0700

    fix display if no reports

--- static/index.js
+++ static/index.js
@@ -8,29 +8,47 @@ const retrieve = async (event) => {
     const response = await fetch("/reports");
     const reports = await response.json();
 
-    const reportsEl = document.getElementById("reports");
-    const tbody = reportsEl.getElementsByTagName("tbody")[0];
-    tbody.innerText = "";
+    if (reports.length) {
+        const reportsEl = document.getElementById("reports");
+        reportsEl.innerText = '';
+        
+        const thead = document.createElement('thead');
+        reportsEl.appendChild(thead);
+        
+        const theadTr = document.createElement('tr');
+        thead.appendChild(theadTr);
+        
+        const typeHeading = document.createElement('th');
+        typeHeading.appendChild(document.createTextNode("Report Type"));
+        theadTr.appendChild(typeHeading);
+        
+        const timeHeading = document.createElement('th');
+        timeHeading.appendChild(document.createTextNode("Creation Timestamp"));
+        theadTr.appendChild(timeHeading);
+        
+        const tbody = document.createElement('tbody');
+        reportsEl.appendChild(tbody);
 
-    reports.forEach(report => {
-        const [name, ts] = report.replace('.csv','').split('-');
-        const date = new Date(ts * 1000);
-        const entry = document.createElement('tr');
-        
-        const nameCell = document.createElement('td');
-        const anchor = document.createElement('a');
-        anchor.appendChild(document.createTextNode(`${name}`));
-        anchor.setAttribute("href",`report/${report}`);
-        nameCell.appendChild(anchor);
-        
-        const timeCell = document.createElement('td');
-        timeCell.appendChild(document.createTextNode(`${date.toISOString()}`));
-        
-        entry.appendChild(nameCell);
-        entry.appendChild(timeCell);
-        
-        tbody.appendChild(entry);
-    });
+        reports.forEach(report => {
+            const [name, ts] = report.replace('.csv','').split('-');
+            const date = new Date(ts * 1000);
+            const entry = document.createElement('tr');
+            
+            const nameCell = document.createElement('td');
+            const anchor = document.createElement('a');
+            anchor.appendChild(document.createTextNode(`${name}`));
+            anchor.setAttribute("href",`report/${report}`);
+            nameCell.appendChild(anchor);
+            
+            const timeCell = document.createElement('td');
+            timeCell.appendChild(document.createTextNode(`${date.toISOString()}`));
+            
+            entry.appendChild(nameCell);
+            entry.appendChild(timeCell);
+            
+            tbody.appendChild(entry);
+        });
+    };
 };
 
 addEventListener("load", () => {
