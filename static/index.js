fdf782f1d067a535be439d03352cf00b9b893f80
Justin Thomas <justin@jdt.io>
2023-07-17 17:31:23 -0700

    add some basic styling and kubernetes artifacts

--- static/index.js
+++ static/index.js
@@ -1,31 +1,48 @@
-const httpRequest = new XMLHttpRequest();
-
-httpRequest.onreadystatechange = (event) => {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        const response = JSON.parse(httpRequest.responseText);
-
-        const reports = document.getElementById("reports");
-        reports.innerText = "";
-
-        response.forEach(report => {
-            const [name, ts] = report.replace('.csv','').split('-');
-            const date = new Date(ts * 1000);
-            const entry = document.createElement('li');
-            const anchor = document.createElement('a');
-            anchor.appendChild(document.createTextNode(`${name} ${date.toISOString()}`));
-            anchor.setAttribute("href",`report/${report}`);
-            entry.appendChild(anchor);
-            reports.appendChild(entry);
-        });
-    }
-};
-
 const sleep = duration => new Promise(resolve => setTimeout(resolve, duration));
+
 const poll = (promiseFn, duration) => {
     promiseFn().then(sleep(duration).then(() => poll(promiseFn, duration)));
 }
 
-poll(() => new Promise(() => {
-    httpRequest.open("GET", "/reports", true);
-    httpRequest.send();
-}), 10000);
+const retrieve = async (event) => {
+    const response = await fetch("/reports");
+    const reports = await response.json();
+
+    const reportsEl = document.getElementById("reports");
+    reportsEl.innerText = "";
+
+    reports.forEach(report => {
+        const [name, ts] = report.replace('.csv','').split('-');
+        const date = new Date(ts * 1000);
+        const entry = document.createElement('li');
+        const anchor = document.createElement('a');
+        anchor.appendChild(document.createTextNode(`${name} ${date.toISOString()}`));
+        anchor.setAttribute("href",`report/${report}`);
+        entry.appendChild(anchor);
+        reportsEl.appendChild(entry);
+    });
+};
+
+addEventListener("load", () => {
+    poll(() => new Promise(() => {
+        retrieve();
+    }), 10000);
+});
+
+const dragOverHandler = (event) => {
+    event.preventDefault();
+    event.target.classList.add("dragged");
+}
+
+const dropHandler = (event) => {
+    event.preventDefault();
+
+    const fileInput = document.getElementById("file");
+    fileInput.files = event.dataTransfer.files;
+    event.target.classList.remove("dragged");
+}
+
+const dragLeaveHandler = (event) => {
+    event.preventDefault();
+    event.target.classList.remove("dragged");
+}
